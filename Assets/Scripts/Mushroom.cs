581f7ce6c7998e7d4c8fb4e786ac77068b5bbc34
WilliamHeidemann <williamskou2227@gmail.com>
2023-01-14 22:11:02 +0100

    update

--- Assets/Scripts/Mushroom.cs
+++ Assets/Scripts/Mushroom.cs
@@ -1,13 +1,24 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using DefaultNamespace;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class Mushroom : MonoBehaviour, IInteractable
 {
+    private MushroomHighController _controller;
+    public float Strength { get; set; }
+
+    private void Start()
+    {
+        _controller = MushroomHighController.Instance;
+        Strength = Random.value * 0.2f + 0.1f;
+    }
+
     public void Interact()
     {
-        Debug.Log("Eating mushroom");
+        StartCoroutine(_controller.ConsumeMushroom(Strength));
         Destroy(gameObject);
     }
 }
---
+++ Assets/Scripts/MushroomHighController.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MushroomHighController : MonoBehaviour
+{
+    [SerializeField] private FloatValue high; // Percentage (0% - 100%)
+
+    public static MushroomHighController Instance;
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    private void Start()
+    {
+        StartCoroutine(SoberUp());
+    }
+
+    private void Update()
+    {
+        print(high.value);
+    }
+
+    private IEnumerator SoberUp()
+    {
+        while (true)
+        {
+            yield return new WaitUntil(IsHigh);
+            high.value -= 0.001f;
+            yield return new WaitForSeconds(0.1f);
+        }
+    }
+    private bool IsHigh() => high.value > 0;
+
+    public IEnumerator ConsumeMushroom(float strength)
+    {
+        float given = 0;
+        while (given < strength)
+        {
+            high.value += 0.05f;
+            given += 0.05f;
+            yield return new WaitForSeconds(0.1f);
+        }
+    }
+}
